da6ee7fcfb1b2d227ece708b130f707e583339f2
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-02-05 20:34:41 +0530

    Update settings.py

--- newb/settings.py
+++ newb/settings.py
@@ -101,12 +101,12 @@ WSGI_APPLICATION = 'newb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
@@ -177,3 +177,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # "http://127.0.0.1:8000",
 # ]
 CORS_ALLOW_ALL_ORIGINS = True
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
